c0b3c5c1e1f185b1977fc7d64f2558bc6c776d76
Rosifaarias <rosilene.farias00@gmail.com>
2025-01-27 09:39:58 -0300

    primeiro commit

---
+++ src/main/java/com/generation/crudfarmacia/CurdfarmaciaApplication.java
@@ -0,0 +1,13 @@
+package com.generation.crudfarmacia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurdfarmaciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurdfarmaciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/crudfarmacia/CurdfarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.crudfarmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurdfarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
